46e3d5c2abe465140480bbae7352b7c7fc260818
Jake Schwartz <jschwartz@figure.com>
2023-03-07 12:24:09 -0800

    Complete contract documentation

--- src/contract.rs
+++ src/contract.rs
@@ -7,6 +7,19 @@ use crate::types::core::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response};
 use provwasm_std::{ProvenanceMsg, ProvenanceQuery};
 
+/// The entry point used when an account instantiates a stored code wasm payload of this contract on
+/// the Provenance Blockchain.
+///
+/// # Parameters
+///
+/// * `deps` A dependencies object provided by the cosmwasm framework.  Allows access to useful
+/// resources like contract internal storage and a querier to retrieve blockchain objects.
+/// * `env` An environment object provided by the cosmwasm framework.  Describes the contract's
+/// details, as well as blockchain information at the time of the transaction.
+/// * `info` A message information object provided by the cosmwasm framework.  Describes the sender
+/// of the instantiation message, as well as the funds provided as an amount during the transaction.
+/// * `msg` A custom instantiation message defined by this contract for creating the initial
+/// configuration used by the contract.
 #[entry_point]
 pub fn instantiate(
     deps: DepsMut<ProvenanceQuery>,
@@ -17,6 +30,20 @@ pub fn instantiate(
     instantiate_contract(deps, env, info, msg)
 }
 
+/// The entry point used when an account initiates an execution process defined in the contract.
+/// This defines the primary purposes of the contract.
+///
+/// # Parameters
+///
+/// * `deps` A dependencies object provided by the cosmwasm framework.  Allows access to useful
+/// resources like contract internal storage and a querier to retrieve blockchain objects.
+/// * `env` An environment object provided by the cosmwasm framework.  Describes the contract's
+/// details, as well as blockchain information at the time of the transaction.
+/// * `info` A message information object provided by the cosmwasm framework.  Describes the sender
+/// of the instantiation message, as well as the funds provided as an amount during the transaction.
+/// * `msg` A custom execution message enum defined by this contract to allow multiple different
+/// processes to be defined for the singular execution route entry point allowed by the
+/// cosmwasm framework.
 #[entry_point]
 pub fn execute(
     deps: DepsMut<ProvenanceQuery>,
@@ -31,6 +58,18 @@ pub fn execute(
     }
 }
 
+/// The entry point used when an account invokes the contract to retrieve information.  Allows
+/// access to the internal storage information in an immutable manner.
+///
+/// # Parameters
+///
+/// * `deps` A dependencies object provided by the cosmwasm framework.  Allows access to useful
+/// resources like contract internal storage and a querier to retrieve blockchain objects.
+/// * `_env` An environment object provided by the cosmwasm framework.  Describes the contract's
+/// details, as well as blockchain information at the time of the transaction.  Unused by this
+/// function, but required by cosmwasm for successfully defined query entrypoint.
+/// * `msg` A custom query message enum defined by this contract to allow multiple different results
+/// to be determined for this route.
 #[entry_point]
 pub fn query(
     deps: Deps<ProvenanceQuery>,
@@ -42,6 +81,18 @@ pub fn query(
     }
 }
 
+/// The entry point used when the contract admin migrates an existing instance of this contract to
+/// a new stored code instance on chain.
+///
+/// # Parameters
+///
+/// * `deps` A dependencies object provided by the cosmwasm framework.  Allows access to useful
+/// resources like contract internal storage and a querier to retrieve blockchain objects.
+/// * `_env` An environment object provided by the cosmwasm framework.  Describes the contract's
+/// details, as well as blockchain information at the time of the transaction.  Unused by this
+/// function, but required by cosmwasm for successfully defined migration entrypoint.
+/// * msg` A custom migrate message enum defined by this contract to allow multiple different
+/// results of invoking the migrate endpoint.
 #[entry_point]
 pub fn migrate(
     deps: DepsMut<ProvenanceQuery>,
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,6 @@
-//!
+//! Additional functionality that does not strictly belong to a category.  Global helpers.
 
+/// Utility functions that assist in Provenance Blockchain communication and data parsing.
 pub mod prov_helpers;
+/// Utility functions that assist in performing common tasks needed by routes.
 pub mod route_helpers;
