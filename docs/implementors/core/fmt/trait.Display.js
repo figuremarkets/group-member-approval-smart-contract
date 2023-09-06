5b77331acae763534619a04d0f5a1ba7abc3380c
hyperschwartz <hyperschwartz@users.noreply.github.com>
2023-09-06 22:44:09 +0000

    Deploying to gh-pages from @ FigureTechnologies/group-member-approval-smart-contract@a84ec193c3af83af90d76cb56fd6c5a5d805c646 🚀

--- docs/implementors/core/fmt/trait.Display.js
+++ docs/implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"group_member_approval_smart_contract":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"group_member_approval_smart_contract/types/core/error/enum.ContractError.html\" title=\"enum group_member_approval_smart_contract::types::core::error::ContractError\">ContractError</a>"]]
+"group_member_approval_smart_contract":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"group_member_approval_smart_contract/types/core/error/enum.ContractError.html\" title=\"enum group_member_approval_smart_contract::types::core::error::ContractError\">ContractError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
